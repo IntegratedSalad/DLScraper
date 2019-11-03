1be0e39b7903a38dc92d3c63dc5fc0a0fa23cc84
Raven <worms3d4@tlen.pl>
2019-11-03 01:26:29 +0100

    Getting word count.
    Sorted list is done.

--- web_app/scripts/find_words.py
+++ web_app/scripts/find_words.py
@@ -3,7 +3,7 @@ from get_soup import make_soup_from_band_name, make_soup_from_album_url
 from time import sleep
 
 #band = input("Type a band name: ")
-soup = make_soup_from_band_name("gorgasm")
+soup = make_soup_from_band_name("cannibal corpse")
 
 def get_albums(soup):
 
@@ -11,7 +11,6 @@ def get_albums(soup):
 
 	album_links = []
 	albums_divs = soup.find_all("div", "album")
-
 
 	for tag in albums_divs:
 		a = tag.a
@@ -32,7 +31,7 @@ def parse_albums(albums_list):
 
 		lyrics_ = album_soup.find_all("div", "lyrics")
 		for lyr in lyrics_:
-			to_remove = lyr.find_all(["h3", "br", "div", "a"])
+			to_remove = lyr.find_all(["h3", "br", "div", "a", "i"])
 			for element in to_remove:
 				element.decompose()
 
@@ -40,7 +39,30 @@ def parse_albums(albums_list):
 
 	return lyrics
 
+def return_word_count(album_urls, album_lyrics):
+	ignore_list = ["the", "my", "to", "from", "this", "i", "you", "and", "for", "with", "your", "a", "an", "or", \
+	"out", "in", "as", "of", "on", "will", "into", "onto", "them", "off", "are", "is", "it", "every", "have", "no", "yes", "by", "it's", \
+	"their", "me", "now", "you'll", "i'll", "will", "but", "can", "we", "us", "our", "not", "could", "would", "am", "don't", "be", "has",\
+	"never", "ever", "sometimes", "like", "i'm", "-", "at", "own"]
+
+	occurences_dict = dict()
+
+	for lyric in album_lyrics:
+		words = lyric.split()
+
+		for word in words:
+			if word.lower() not in ignore_list:
+				if word.lower() in occurences_dict:
+					occurences_dict[word.lower()] += 1
+				else:
+					occurences_dict[word.lower()] = 1
+
+	sorted_list = sorted(occurences_dict.items(), key=lambda k: k[1])
+	return sorted_list
+
 album_urls = get_albums(soup)
 album_lyrics = parse_albums(album_urls)
 
-print(len(album_lyrics))
+words = return_word_count(album_urls, album_lyrics)
+print(words)
+
