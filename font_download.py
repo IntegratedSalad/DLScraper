bc5c7c3b5bf87a265dc4c36553cf59e87e3e4300
Raven <worms3d4@tlen.pl>
2019-11-13 21:21:23 +0100

    Added script to download font.

---
+++ font_download.py
@@ -0,0 +1,23 @@
+import requests
+import os
+from zipfile import ZipFile
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+dirs = [PATH, "web_app", "static"]
+
+LINK = "https://dl.dafont.com/dl/?f=augusta"
+
+def main():
+
+	"""Downloads 'Augusta' font from dafont.com, and places it in static folder."""
+
+	request = requests.get(LINK, allow_redirects=True)
+	with open(os.path.join(PATH, "archive.zip"), 'wb') as file:
+		file.write(request.content)
+
+	with ZipFile('archive.zip', 'r') as zip_file:
+		zip_file.extractall(path=os.path.join(*dirs))
+
+
+if __name__ == '__main__':
+	main()
