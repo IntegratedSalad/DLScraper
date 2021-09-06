01df1767d96816cb175d028ee4369440f259078b
Buppo <worms3d4@tlen.pl>
2021-09-06 13:19:46 +0200

    Showing that user provided invalid name

--- web_app/routes.py
+++ web_app/routes.py
@@ -6,16 +6,20 @@ from web_app import get_stats
 @app.route("/home.html", methods=["GET", "POST"])
 def home():
 
+	invalid_band = False
+
 	if request.method == "POST":
 		band_name = request.form['band']
 		if get_stats.return_stats(band_name) == "albums_not_found":
-			# TODO make something
 			print("Albums not found")
+		elif get_stats.return_stats(band_name) == "invalid_band":
+			invalid_band = True
 		else:
 			return redirect("result.html")
 
-	return render_template('home.html')
+	return render_template('home.html', invalid=invalid_band)
 
 @app.route("/result.html", methods=["GET", "POST"])
 def results():
 	return render_template("result.html")
+	
